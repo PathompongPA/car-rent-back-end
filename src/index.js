16e320371751fbba145ec5f27a22321081ef7eb1
l.pathompong <pathompong.litk@gmail.com>
2025-07-21 23:37:41 +0700

    add(init Content) : add init json content for starter

--- src/index.js
+++ src/index.js
@@ -8,6 +8,24 @@ app.listen(app_port, async () => {
         model.initSql.connect()
         await model.sequelize.sync()
         console.log("port is : ", app_port);
+        const amountContetn = await model.CONTENT.count()
+        if (count === 0) {
+            await Role.bulkCreate([
+                { "id": "contact.card", "value": [{ "list": [{ "link": "", "title": "" },], "title": "" },] },
+                { "id": "contact.title", "value": [{ "text": "" }] },
+                { "id": "footer.addess", "value": { "text": "" } },
+                { "id": "journeyBooking.card", "value": [{ "title": "", "detail": [""] },] },
+                { "id": "journeyBooking.title", "value": { "text": "" } },
+                { "id": "logo", "value": { "image": "" } },
+                { "id": "navbar.title", "value": [{ "link": "", "text": "" }] },
+                { "id": "Qa.card", "value": [{ "answer": "", "question": "" },] },
+                { "id": "Qa.title", "value": { "text": "" } },
+                { "id": "viewBoard.image", "value": [] }
+            ])
+            console.log('create json content in db')
+        } else {
+            console.log('Table has json content in db, Skipping')
+        }
     } catch (error) {
         console.log(error);
     }
